92f12c63e37fa03dc54a3c95dcff5258a7e48db5
minseojun <89567771+minseojun@users.noreply.github.com>
2026-02-21 16:58:47 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
+# app.py
 import streamlit as st
 
 from failog.ui import inject_css, render_hero
 from failog.db import init_db
-from failog.user import get_or_create_user_id
+from failog.user_id import get_or_create_user_id
 from failog.nav import top_nav
 from failog.screens_planner import screen_planner
 from failog.screens_failures import screen_failures
